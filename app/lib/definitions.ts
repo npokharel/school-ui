dac8275f2b80fc8363758310fb252c54942d7b72
npokharel <niraj.pokharel@gmail.com>
2023-12-22 22:20:26 +0545

    initial commit

---
+++ app/lib/definitions.ts
@@ -0,0 +1,24 @@
+// This file contains type definitions of data.
+
+import NextAuth, { DefaultSession, DefaultJWT } from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    userId: string;
+  } 
+}
+
+export type User = {
+    id?: string | null;
+    name?: string | null;
+    email?: string | null;
+  }
